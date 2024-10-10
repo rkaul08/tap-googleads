d62e0f390a81766e47b059827c123db208956abb
Tobias Cadée <cadeetobias@gmail.com>
2024-10-10 15:01:35 +0200

    feat: Implement parent-child sync for all accessible customers (#45)
    * feat(): fix the parent-child logic
    
    At the moment `AccessibleCustomers` is not being used as a parent stream to `CustomerHierarchyStream`. This update fixes that. By updating to the latest singer-sdk version we can use `generate_child_contexts` to generate multiple child streams per parent stream to sync all accessible customers.
    
    I also added a check to make sure a customer is enabled to avoid 403 errors.
    
    Also changed some of the tap config because in my opinion it was a bit unclear. `login_customer_id` is now required as it is required in the header. You can also specify `customer_ids` to filter on customers you want to sync. But only if you have access to them and they are enabled. I understand that some people need to update their config but in the end I think this is more clear.
    
    Moved pagination to the new singer-sdk pagination setup: https://sdk.meltano.com/en/v0.40.0/guides/pagination-classes.html
    
    Other updates are linting updates.
    
    * Update tap_googleads/tap.py
    
    Co-authored-by: ReubenFrankel <60552974+ReubenFrankel@users.noreply.github.com>
    
    * Update tap_googleads/streams.py
    
    Co-authored-by: ReubenFrankel <60552974+ReubenFrankel@users.noreply.github.com>
    
    * Maintain support for `customer_id` config
    
    * Remove unused import
    
    * Update README with info on `customer_ids`/`customer_id`
    
    * Fix tests broken by SDK version bump and config
    
    * Add test workflow
    
    * Update locked dependencies
    
    ---------
    
    Co-authored-by: ReubenFrankel <60552974+ReubenFrankel@users.noreply.github.com>
    Co-authored-by: Reuben Frankel <rfrankel@matatika.com>

--- README.md
+++ README.md
@@ -35,16 +35,16 @@ Settings required to run this tap.
 - `oauth_credentials.client_secret` (required)
 - `oauth_credentials.refresh_token` (required)
 - `developer_token` (required)
-- `customer_id` (required)
+- `customer_ids` (optional) Array of ids: ["123", "456", "789"]
+- `customer_id` (optional)
 - `login_customer_id` (optional)
 - `start_date` (optional)
 - `end_date` (optional)
-- `comma_separated_string_of_customer_ids` (optional) String of comma separated ids: `123, 456, 789`
 - `enable_click_view_report_stream` (optional) Boolean, Default is `False`
 
-If using a manager account, `login_customer_id` should be set to the customer ID of the manager account while `customer_id` should be set to the customer ID of the account you want to sync.
-
-If you provide `comma_separated_string_of_customer_ids`, you are overriding what customer_id(s) to get data for.
+If using a manager account, `login_customer_id` should be set to the customer ID of the manager account.
+ 
+If you provide `customer_ids`, you filter on which customer accounts you want to get data for. The same is true for `customer_id` but for a single customer account. If both are provided, `customer_ids` takes precedence. If neither are provided, all customer accounts available to the authenticated principal are synced. 
 
 How to get these settings can be found in the following Google Ads documentation:
 
--- tap_googleads/tap.py
+++ tap_googleads/tap.py
@@ -43,45 +43,85 @@ class TapGoogleAds(Tap):
     # TODO: Add Descriptions
     config_jsonschema = th.PropertiesList(
         th.Property(
-            "oauth_credentials.client_id",
-            th.StringType,
-        ),
-        th.Property(
-            "oauth_credentials.client_secret",
-            th.StringType,
+            "oauth_credentials",
+            th.OneOf(
+                th.ObjectType(
+                    th.Property(
+                        "client_id",
+                        th.StringType,
+                        required=True,
+                    ),
+                    th.Property(
+                        "client_secret",
+                        th.StringType,
+                        required=True,
+                        secret=True,
+                    ),
+                    th.Property(
+                        "refresh_token",
+                        th.StringType,
+                        required=True,
+                        secret=True,
+                    ),
+                    additional_properties=False,
+                ),
+                th.ObjectType(
+                    th.Property(
+                        "refresh_proxy_url",
+                        th.StringType,
+                        required=True,
+                    ),
+                    th.Property(
+                        "refresh_proxy_url_auth",
+                        th.StringType,
+                        secret=True,
+                    ),
+                    additional_properties=False,
+                ),
+            ),
+            required=True,
         ),
         th.Property(
             "developer_token",
             th.StringType,
-        ),
-        th.Property(
-            "oauth_credentials.refresh_token",
-            th.StringType,
-        ),
-        th.Property(
-            "customer_id",
-            th.StringType,
+            required=True,
+            secret=True,
         ),
         th.Property(
             "login_customer_id",
             th.StringType,
-            description="Value to use in the login-customer-id header, if different from the customer_id to sync. Useful if you are syncing using a manager account.",
+            description="Value to use in the login-customer-id header if using a manager customer account. See https://developers.google.com/search-ads/reporting/concepts/login-customer-id for more info.",
         ),
         th.Property(
-            "comma_separated_string_of_customer_ids",
+            "customer_ids",
+            th.ArrayType(th.StringType),
+            description="Get data for the provided customers only, rather than all accessible customers. Takes precedence over `customer_id`.",
+        ),
+        th.Property(
+            "customer_id",
             th.StringType,
-            description="Overrides the taps default get all data for all available customers logic, and will get you the data for only the the provided customer_ids",
+            description="Get data for the provided customer only, rather than all accessible customers. Superseeded by `customer_ids`.",
+        ),
+        th.Property(
+            "start_date",
+            th.DateTimeType,
+            description="Start date for all of the streams that use date based filtering.",
+        ),
+        th.Property(
+            "end_date",
+            th.DateTimeType,
+            description="End date for all of the streams that use date based filtering.",
         ),
         th.Property(
             "enable_click_view_report_stream",
             th.BooleanType,
             description="Enables the tap's ClickViewReportStream. This requires setting up / permission on your google ads account(s)",
+            default=False,
         ),
     ).to_dict()
 
     def discover_streams(self) -> List[Stream]:
         """Return a list of discovered streams."""
-        if self.config.get("enable_click_view_report_stream"):
-            if self.config.get("enable_click_view_report_stream") == True:
-                STREAM_TYPES.append(ClickViewReportStream)
+        if self.config["enable_click_view_report_stream"]:
+            STREAM_TYPES.append(ClickViewReportStream)
         return [stream_class(tap=self) for stream_class in STREAM_TYPES]
